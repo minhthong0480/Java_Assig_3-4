aae8bfe1e636753cae76517c954b911a9029994e
Minh Thong <s3852882@rmit.edu.vn>
2022-03-28 16:36:03 +0700

    create main file and class

---
+++ Main.java
@@ -0,0 +1,13 @@
+class Student{
+    public String StudentID;
+    public String StudentName;
+    public String BirthDate;
+}
+
+class Course{
+    public String CourseID;
+    public String CourseName;
+    public int NumberofCredit;
+}
+public class Main {
+}
