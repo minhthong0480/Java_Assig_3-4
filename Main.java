2f86765095af3f801f5f584f79507b5a2f899735
Minh Thong <s3852882@rmit.edu.vn>
2022-03-29 11:42:33 +0700

    create constructor

--- Main.java
+++ Main.java
@@ -2,12 +2,34 @@ class Student{
     public String StudentID;
     public String StudentName;
     public String BirthDate;
+
+    public Student(String StudentID, String StudentName, String BirthDate) {
+        this.StudentID = StudentID;
+        this.StudentName = StudentName;
+        this.BirthDate = BirthDate;
+    }
 }
 
 class Course{
     public String CourseID;
     public String CourseName;
     public int NumberofCredit;
+
+    public Course(String CourseID, String CourseName, int NumberofCredit){
+        this.CourseID = CourseID;
+        this.CourseName = CourseName;
+        this.NumberofCredit = NumberofCredit;
+    }
+}
+
+class StudentEnrolment{
+    public Student student;
+    public Course course;
+    public String semester;
+
 }
 public class Main {
+    public static void main(String[] args) {
+
+    }
 }
